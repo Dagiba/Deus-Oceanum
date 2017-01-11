f0a16d87c23638ca5430d46169bf16418200ed2c
miller <fasha1000@gmail.com>
2017-01-10 22:24:24 -0500

    changed all cubes/obstacle to instantiate runtime(except 1)

---
+++ assets/Scripts/ObstacleInstantiate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleInstantiate : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject boxPrefab;
+
+    private GameObject clonePrefab;
+
+	void Start () {
+        ObstacleInstantiateHelper(boxPrefab, "ObstacleBox");
+        //boxPrefab.transform.eulerAngles = new Vector3(-90, 0, 0); 
+        //clonePrefab.transform.eulerAngles = new Vector3(-90, 0, 0);
+    }
+
+    private void ObstacleInstantiateHelper(GameObject prefab, string tag)
+    {
+        GameObject[] allObj = GameObject.FindGameObjectsWithTag(tag);
+        foreach (GameObject cloneObst in allObj)
+        {
+            clonePrefab = Instantiate(prefab, cloneObst.transform.position, Quaternion.identity) as GameObject;
+        }
+    }
+	
+}
